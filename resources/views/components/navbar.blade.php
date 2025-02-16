6483c8447058957c56b79c7f8c5c65d37a4d0d34
deknaa <kdkrisnaaa@gmail.com>
2025-02-16 18:47:06 +0800

    first commit

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,17 @@
+<nav class="flex items-center justify-between p-10">
+    <div class="logo">
+        <h1>{{ config('app.name') }}</h1>
+    </div>
+    <div>
+        <ul class="flex gap-7">
+            <li><a href="#" class="hover:underline">Home</a></li>
+            <li><a href="#" class="hover:underline">Our Services</a></li>
+            <li><a href="#" class="hover:underline">Testimony</a></li>
+            <li><a href="#" class="hover:underline">Products</a></li>
+            <li><a href="#" class="hover:underline">About Us</a></li>
+        </ul>
+    </div>
+    <div>
+        <a href="#" class="px-5 py-3 transition duration-300 ease-in-out border border-green-600 rounded outline-4 hover:bg-green-700 hover:text-white">Sign Up</a>
+    </div>
+</nav>
